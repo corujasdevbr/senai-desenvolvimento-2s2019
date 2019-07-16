d92011c3ed53c728189ed79b57624099cd9a8e06
Helena Strada <h.strada@hotmail.com>
2019-07-16 12:33:44 -0300

    Alterado roteiro

--- sprint-2-backend/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Startup.cs
+++ sprint-2-backend/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Startup.cs
@@ -20,6 +20,11 @@ namespace Senai.Sstop.WebApi
             {
                 options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "SStop API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +35,13 @@ namespace Senai.Sstop.WebApi
                 app.UseDeveloperExceptionPage();
             }
             app.UseCors("CorsPolicy");
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SStop API V1");
+            });
 
             app.UseMvc();
 
