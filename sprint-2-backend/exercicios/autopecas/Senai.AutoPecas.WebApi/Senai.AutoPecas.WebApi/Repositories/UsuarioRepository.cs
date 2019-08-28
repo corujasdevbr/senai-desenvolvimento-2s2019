b0979ce6af02450903efc4b015f661b3c0ca4fc4
unknown <admin.local@TLAB081.info.local>
2019-08-28 17:12:55 -0300

    up

---
+++ sprint-2-backend/exercicios/autopecas/Senai.AutoPecas.WebApi/Senai.AutoPecas.WebApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.AutoPecas.WebApi.Repositories
+{
+    public class UsuarioRepository
+    {
+
+    }
+}
---
+++ sprint-2-backend/exercicios/autopecas/script_autopecas.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE AutoPecas;
+USE AutoPecas;
+
+CREATE TABLE Usuarios
+(
+	UsuarioId		INT PRIMARY KEY IDENTITY
+	, Email			VARCHAR(255) NOT NULL UNIQUE
+	, Senha			VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Fornecedores 
+(
+	FornecedorId	INT PRIMARY KEY IDENTITY
+	, CNPJ			CHAR(14)		NOT NULL UNIQUE
+	, RazaoSocial	VARCHAR(255)	NOT NULL UNIQUE
+	, NomeFantasia	VARCHAR(255)	NOT NULL
+	, Endereco		VARCHAR(255)	NOT NULL
+	, UsuarioId		INT FOREIGN KEY REFERENCES Usuarios(UsuarioId) NOT NULL UNIQUE
+);
+
+CREATE TABLE Pecas (
+	PecaId			INT PRIMARY KEY IDENTITY
+	, Codigo		VARCHAR(255) NOT NULL UNIQUE
+	, Descricao		TEXT
+	, Peso			FLOAT(2) NOT NULL
+	, PrecoCusto	FLOAT(2) NOT NULL
+	, PrecoVenda	FLOAT(2) NOT NULL
+	, FornecedorId	INT FOREIGN KEY REFERENCES Fornecedores(FornecedorId) NOT NULL
+);
