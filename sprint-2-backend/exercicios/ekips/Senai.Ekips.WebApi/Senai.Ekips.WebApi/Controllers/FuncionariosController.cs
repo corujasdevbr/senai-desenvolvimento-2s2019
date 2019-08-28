30a90db649c736a9815e39c9ce2e60bde097f0fc
unknown <admin.local@TLAB081.info.local>
2019-08-28 08:35:14 -0300

    up

---
+++ sprint-2-backend/exercicios/ekips/Senai.Ekips.WebApi/Senai.Ekips.WebApi/Controllers/FuncionariosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Ekips.WebApi.Repositories;
+
+namespace Senai.Ekips.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class FuncionariosController : ControllerBase
+    {
+        FuncionarioRepository FuncionarioRepository = new FuncionarioRepository();
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            string permissao = HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Role).Value;
+            if (permissao == "ADMINISTRADOR")
+                return Ok(FuncionarioRepository.Listar());
+            else if (permissao == "COMUM")
+                return Ok(FuncionarioRepository.BuscarFuncionarioPorUsuario(Convert.ToInt32(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value)));
+            else
+                return Forbid();
+        }
+    }
+}
---
+++ sprint-2-backend/exercicios/ekips/Senai.Ekips.WebApi/Senai.Ekips.WebApi/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,27 @@
+﻿using Senai.Ekips.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Ekips.WebApi.Repositories
+{
+    public class FuncionarioRepository
+    {
+        public List<Funcionarios> Listar()
+        {
+            using (EkipsContext ctx = new EkipsContext())
+            {
+                return ctx.Funcionarios.ToList();
+            }
+        }
+
+        public Funcionarios BuscarFuncionarioPorUsuario (int id)
+        {
+            using (EkipsContext ctx = new EkipsContext())
+            {
+                return ctx.Funcionarios.FirstOrDefault(x => x.Usuario.UsuarioId == id);
+            }
+        }
+    }
+}
