035197500bb5e2b1fb6c7e87cb8a1c829eea35d2
Erik Vitelli <erik.vitelli1@gmail.com>
2019-09-09 09:50:36 -0300

    Manual Peças

---
+++ sprint-2-backend/exercicios/manualpecas/BD/ManualPecas_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO Fornecedores (Nome, CNPJ, Senha)
+VALUES ('Fornecedor A', '12.345.678/0001-90', '123456')
+
+INSERT INTO Fornecedores (Nome, CNPJ,Senha)
+VALUES ('Fornecedor B', '09.876.543/0001-21', '123456')
+
